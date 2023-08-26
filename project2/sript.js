764347b99474e8ca7653794083a60c7d242f8058
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-08-26 19:45:32 +0530

    timer

---
+++ project2/sript.js
@@ -0,0 +1,6 @@
+	function changeColor() {
+	  var color = document.getElementById("myInput").value;
+	  document.body.style.backgroundColor = color;
+      document.getElementById("myInput").value = "";
+
+	}
