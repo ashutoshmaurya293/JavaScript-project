5f423cf2ca12b70cc635bb19aebeae89829d7eb5
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-21 06:00:45 +0530

    adding question

--- project 11/script.js
+++ project 11/script.js
@@ -1,4 +1,37 @@
- const addTask= ()=>{
-console.log("hellow word!");
-}
- 
+const quizData = [
+{
+    question:"what does HTML stand for",
+    Option:[
+        "Hypertext Markup Language",
+        "Hyper Transfre Markup Language",
+        'Hypertext Machine Language',
+        "Hyperlink and Text Markup Language"
+    ],
+    currect:0,
+},
+{
+    question:"Which CSS property is used to control the spacing between element",
+    Option:["margine", "padding", "spacing","border-spacing"],
+    correct:0,
+},
+{
+    question:"what is the javaScript function used to select an HTML element by its id ?",
+    Option:[
+        "document.query",
+        "getElementById",
+        "selectElement",
+        "findElementById",
+    ],
+    correct:1,
+},
+{
+    question:"In React js, which hook is used to perform side effect in a function component ?",
+    Option:["useEffect", "usestate", "useContext", "useReducer"],
+correct:0,    
+},
+{
+    question:"Which HTML tag is used to create an orderd list ?", 
+    Option: ["<ul>", "<li>", "<ol>","<dl>"],
+    correct:2,
+},
+]
