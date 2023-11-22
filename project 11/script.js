ef0c2f6ea5329301bd54aa40ea6a60b1e1666424
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-22 13:36:22 +0530

    adding option section

--- project 11/script.js
+++ project 11/script.js
@@ -1,37 +1,62 @@
 const quizData = [
-{
-    question:"what does HTML stand for",
-    Option:[
-        "Hypertext Markup Language",
-        "Hyper Transfre Markup Language",
-        'Hypertext Machine Language',
-        "Hyperlink and Text Markup Language"
+  {
+    question: "what does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hyper Transfre Markup Language",
+      "Hypertext Machine Language",
+      "Hyperlink and Text Markup Language",
     ],
-    currect:0,
-},
-{
-    question:"Which CSS property is used to control the spacing between element",
-    Option:["margine", "padding", "spacing","border-spacing"],
-    correct:0,
-},
-{
-    question:"what is the javaScript function used to select an HTML element by its id ?",
-    Option:[
-        "document.query",
-        "getElementById",
-        "selectElement",
-        "findElementById",
+    currect: 0,
+  },
+  {
+    question:
+      "Which CSS property is used to control the spacing between element",
+    options: ["margine", "padding", "spacing", "border-spacing"],
+    correct: 0,
+  },
+  {
+    question:
+      "what is the javaScript function used to select an HTML element by its id ?",
+    options: [
+      "document.query",
+      "getElementById",
+      "selectElement",
+      "findElementById",
     ],
-    correct:1,
-},
-{
-    question:"In React js, which hook is used to perform side effect in a function component ?",
-    Option:["useEffect", "usestate", "useContext", "useReducer"],
-correct:0,    
-},
-{
-    question:"Which HTML tag is used to create an orderd list ?", 
-    Option: ["<ul>", "<li>", "<ol>","<dl>"],
-    correct:2,
-},
-]
+    correct: 1,
+  },
+  {
+    question:
+      "In React js, which hook is used to perform side effect in a function component ?",
+    options: ["useEffect", "usestate", "useContext", "useReducer"],
+    correct: 0,
+  },
+  {
+    question: "Which HTML tag is used to create an orderd list ?",
+    options: ["<ul>", "<li>", "<ol>", "<dl>"],
+    correct: 2,
+  },
+];
+const answerElm = document.querySelectorAll(".answer");
+const [questionElem, option_1, option_2, option_3, option_4] =
+  document.querySelectorAll(
+    "#question, .option_1, .option_2, .option_3, .option_4"
+  );
+
+const submit = document.getElementById("submit");
+
+const currentQuiz = 0;
+const score = 0;
+
+const Loadquiz = () => {
+  const { question, options } = quizData[currentQuiz];
+  // console.log(options);
+  questionElem.innerText = question;
+options.forEach(
+    (el,i)=>(window[`option_${i+1}`].innerText = el)
+   
+  
+);
+};
+Loadquiz();
