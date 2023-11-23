ae2fd4090272f12893e9c1cef3dc171c7e76efd4
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-23 19:47:14 +0530

    api project

--- project12/script.js
+++ project12/script.js
@@ -1,15 +1,24 @@
 const AccesKey = 'MReg00ZXxoRekpUkEdz7oeNd40XyF4FHo5Hog-KD5X8'
+let searchInput = document.getElementById("searchInput")
+let button = document.getElementById("btn")
 
-const getData = async () => {
+
+const getData = async (searchValue) => {
   try {
     let fech = await fetch(
-      `https://api.unsplash.com/search/photos?query=dog&per_page=28&page=1&client_id=${AccesKey}`
+      `https://api.unsplash.com/search/photos?query=${searchValue}&per_page=28&page=1&client_id=${AccesKey}`
     );
     let jsonData = await fech.json();
-    console.log(jsonData);
+    // console.log(jsonData.results);
+    console.log(searchValue);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
 
-getData();
+// getData();
+
+button.addEventListener("click", function(){
+    let searchValue = searchInput.value
+    getData(searchInput)
+})
