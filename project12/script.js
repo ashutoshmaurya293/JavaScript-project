4e8190a73e46d05d2a7deb1c8ff8196390ae05b2
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-11-24 20:06:08 +0530

    adding more load button

--- project12/script.js
+++ project12/script.js
@@ -1,24 +1,44 @@
-const AccesKey = 'MReg00ZXxoRekpUkEdz7oeNd40XyF4FHo5Hog-KD5X8'
-let searchInput = document.getElementById("searchInput")
-let button = document.getElementById("btn")
+const AccesKey = "MReg00ZXxoRekpUkEdz7oeNd40XyF4FHo5Hog-KD5X8";
+let searchInput = document.getElementById("searchInput");
+let button = document.getElementById("btn");
+let showData = document.getElementById("showData");
+let moreBTN = document.getElementById("moreBTN");
+let page = 1;
 
-
-const getData = async (searchValue) => {
+const getData = async (searchInputValue, pageNo) => {
   try {
     let fech = await fetch(
-      `https://api.unsplash.com/search/photos?query=${searchValue}&per_page=28&page=1&client_id=${AccesKey}`
+      `https://api.unsplash.com/search/photos?query=${searchInputValue}&per_page=28&page=${pageNo}&client_id=${AccesKey}`
     );
     let jsonData = await fech.json();
-    // console.log(jsonData.results);
-    console.log(searchValue);
+    if (searchInput.value == "") {
+      showData.innerHTML = `
+      <h1>pleace search</h1>
+      `;
+    } else {
+      document.querySelector(".more").style.display = "block";
+    }
+    jsonData.results.forEach(function (e) {
+      console.log(e);
+      let div = document.createElement("div");
+      div.classList.add("card");
+      showData.appendChild(div);
+      div.innerHTML = `
+      <img src=${e.urls.small_s3} alt="image">
+<a href=${e.links.html} target = _blank>${e.alt_description}</a>
+      `;
+    });
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 };
 
-// getData();
-
-button.addEventListener("click", function(){
-    let searchValue = searchInput.value
-    getData(searchInput)
-})
+button.addEventListener("click", function () {
+  showData.innerHTML = "";
+  const searchInputValue = searchInput.value;
+  getData(searchInputValue);
+});
+moreBTN.addEventListener("click", function () {
+  const searchInputValue = searchInput.value;
+  getData(searchInputValue, page++);
+});
