d871b8685f7748385aafc90d0605f1915b1c03ea
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-01 20:36:49 +0530

    Calculater app completed

---
+++ project13/script.js
@@ -0,0 +1,26 @@
+let input = document.getElementById("input");
+if ((input.value = "undefined")) {
+  input.value = "";
+}
+let num = "";
+let buttons = document.querySelectorAll("button");
+
+Array.from(
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (e.target.innerHTML == "=") {
+        num = eval(num);
+        document.querySelector("input").value = num;
+      } else if (e.target.innerHTML == "C") {
+        num = num.slice(0, -1);
+        document.querySelector("input").value = num;
+      } else if (e.target.innerHTML == "AC") {
+        num = "";
+        document.querySelector("input").value = num;
+      } else {
+        num = num + e.target.innerHTML;
+        document.querySelector("input").value = num;
+      }
+    });
+  })
+);
