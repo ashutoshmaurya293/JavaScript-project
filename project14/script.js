63dcf340e343fe92792074c1b0c514fb1f085eb5
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-26 19:06:39 +0530

    close icone added

--- project14/script.js
+++ project14/script.js
@@ -1,12 +1,25 @@
 const icon = document.querySelector(".icon");
 const backdrop = document.querySelector(".backdrop");
 const mainDrower = document.querySelector(".mainDrower");
+// const menu = document.querySelector(".icon");
+const hellow = document.querySelector("#hellow");
+let Icon = true;
+function change() {
+    mainDrower.style.display = "none";
+    backdrop.style.display = "none";
+  }
+backdrop.addEventListener("click", change);
+icon.addEventListener("click", function () {
+  mainDrower.style.display = "block";
+  backdrop.style.display = "block";
+//   console.log("hello");
+  if (Icon) {
+    hellow.className = "fa-solid fa-xmark";
+    Icon = !Icon;
+  } else {
+    hellow.className = "fa-solid fa-bars";
+    Icon = !Icon
+    change()
 
-backdrop.addEventListener("click", function(){
-    mainDrower.style.display = "none"
-    backdrop.style.display = "none"
-})
-icon.addEventListener("click", function(){
-    mainDrower.style.display = "block"
-    backdrop.style.display = "block"
-})
+  }
+});
