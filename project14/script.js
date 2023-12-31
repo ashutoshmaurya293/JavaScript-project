e403b56188839f2d05bfaf2ae025f2b0b94a2612
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-31 20:38:19 +0530

    api added

--- project14/script.js
+++ project14/script.js
@@ -20,3 +20,42 @@ function change() {
 }
 icon.addEventListener("click", change);
 backdrop.addEventListener("click", change);
+
+const api = async()=>{
+  const url = 'https://fakestoreapi.com/products'
+ try {
+  const result = await fetch(url).then((res)=>res.json())
+  console.log(result);
+  const fullCart = document.querySelector(".fullCart")
+let innerHTML = ''
+result.forEach((e)=>{
+  innerHTML +=  `
+  <div class="cart">
+  <div class="cartImg">
+    <img
+      src=${e.image}
+      alt=""
+    />
+  </div>
+  <div class="element">
+    <p class="name">${e.title}</p>
+    <p>
+      <span class="price">₹${e.price*8}</span>
+      <span class="price-c">overwords</span>
+    </p>
+    <p class="delivery">Free Delivery</p>
+    <button>Add To Cart</button>
+  </div>
+  </div>
+  `
+})
+fullCart.innerHTML =innerHTML
+ } catch (error) {
+  console.log(error);
+ }
+}
+api()
+
+
+
+
