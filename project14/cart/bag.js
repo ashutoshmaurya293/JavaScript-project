91a4ad2bce295ed1fc170b8b0bfd9750542b0ac1
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-01-02 20:00:28 +0530

    displayBagSummary added

--- project14/cart/bag.js
+++ project14/cart/bag.js
@@ -4,7 +4,38 @@ onLoad();
 
 function onLoad() {
   loadBagItemObjects();
+  displayBagSummary()
    
+}
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector('.bag-summary');
+
+
+  bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS ({totalItem} Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">₹{totalMRP}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-₹{totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">₹99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">₹{finalPayment}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>
+  `;
 }
 function loadBagItemObjects(){
 //   console.log(bagItems);
@@ -66,7 +97,6 @@ function removeFromBag(itemId) {
   localStorage.setItem('bagItems', JSON.stringify(bagItems));
   loadBagItemObjects();
   bagIcon()
-  // displayBagItems();
   load()
-  // displayBagSummary();
+
 }
