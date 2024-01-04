62d0ff47868b20d35d3978e2e4b943ceb1e1f896
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-01-04 13:24:57 +0530

    single page styling

--- project14/singlePage/script.js
+++ project14/singlePage/script.js
@@ -1,1 +1,13 @@
-console.log(singleItem);
+// console.log(singleItem);
+// let main = document.getElementById("main")
+
+// const api = async () => {
+//     const url = `https://fakestoreapi.com/products/${singleItem}`;
+//       const result = await fetch(url).then((res) => res.json());
+//       console.log(result);
+// if(!main)return
+// main.innerHTML = `
+// <img src=${result.image} alt="" height="400px">
+// <p>hellow</p>`
+// }
+// api()
