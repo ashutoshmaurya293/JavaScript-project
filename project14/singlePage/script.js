8e6077902e77662b098659840bc0e6a9775d83d7
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-01-15 20:53:44 +0530

    single fixed

--- project14/singlePage/script.js
+++ project14/singlePage/script.js
@@ -21,14 +21,14 @@ const api = async () => {
   <div class="description">
     <h2>${result.title}</h2>
     <h4>${result.category}</h4>
-    <p class="rating">
+    <span class="rating">
     ${result.rating.rate}
-
-    
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+  </svg>
     <span class="ratingContent">20143 Ratings, ${result.rating.count} Reviews </span>
-  </p>
-  <p class="delivery">free delivery</p>
+  </span>
+  <span class="delivery">free delivery</span>
     <h1>₹${Math.floor(result.price*8)}</h1>
     <p>${result.description}</p>
     <button onclick = "addToCart(${result.id})">Add to Cart</button>
