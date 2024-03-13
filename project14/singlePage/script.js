4a635c43461e36eba7dbb09182d413d0d48450b4
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2024-03-13 13:12:07 +0530

    similer loader added

--- project14/singlePage/script.js
+++ project14/singlePage/script.js
@@ -1,10 +1,13 @@
 // console.log(singleItem);
 let main = document.getElementById("main");
 let similer = document.getElementById("similer");
+const loaderCartSimiler = document.getElementById("loaderCartSimiler");
+
+// first singleImage Api
 
 const api = async () => {
+  // first loader
   const loader = document.getElementById("loadingDiv");
-
   const url = `https://fakestoreapi.com/products/${singleItem}`;
   const result = await fetch(url).then((res) => res.json());
   // console.log(result);
@@ -27,13 +30,15 @@ const api = async () => {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
   </svg>
-    <span class="ratingContent">20143 Ratings, ${result.rating.count
+    <span class="ratingContent">20143 Ratings, ${
+      result.rating.count
     } Reviews </span>
   </span>
   <span class="delivery">free delivery</span>
     <h1>₹${Math.floor(result.price * 8)}</h1>
     <p>${result.description}</p>
-    <button onclick = "addToCart(${result.id
+    <button onclick = "addToCart(${
+      result.id
     })" id="addButton">Add to Cart</button>
     <button id = "BuyButton">Buy Now</button>
   </div>
@@ -41,10 +46,15 @@ const api = async () => {
   SimilerApi(result?.category);
 };
 api();
+// second similer card Api
 
 const SimilerApi = async (category) => {
+  // second loader
+
+  loaderCartSimiler.style.display = "block";
   const url = `https://fakestoreapi.com/products/category/${category}`;
   const result = await fetch(url).then((res) => res.json());
+  loaderCartSimiler.style.display = "none";
   result.map((e) => {
     similer.innerHTML += ` 
   <div class="cart">
